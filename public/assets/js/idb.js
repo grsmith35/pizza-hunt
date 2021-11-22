e7878aa2f205a5ce16df029858b9da505e4ba213
Riley Smith <gordonrileysmith@gmail.com>
2021-11-21 21:33:43 -0700

    weekend work

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -12,10 +12,56 @@ request.onupgradeneeded = function(event) {
 request.onsuccess = function(event) {
     db = event.target.result;
     if(navigator.onLine) {
-       // uploadPizza();
+       uploadPizza();
     }
 };
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
-}
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    pizzaObjectStore.add(record);
+};
+
+function uploadPizza() {
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    const getAll = pizzaObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/pizzas', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+                const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+                pizzaObjectStore.clear();
+
+                alert('All saved pizzas have been submitted!');
+            })
+            .catch(err => console.log(err))
+        }
+    }
+};
+
+window.addEventListener('online', uploadPizza);
